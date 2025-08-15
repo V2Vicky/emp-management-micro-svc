3056c95361f451f627002135c0c2ecb87f153862
V2Vicky <vignesh.varatharajan03@gmail.com>
2025-08-15 21:20:27 +0530

    Added Config Server

---
+++ config-server/src/test/java/com/vvv/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvv.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
